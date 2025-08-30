984e57c54096698cba2c6dfa387923319d7a776c
Nilay Yadav <nilayyadav10@gmail.com>
2025-08-30 21:48:46 +0530

    homebrew mcpfiy

---
+++ Formula/mcpify.rb
@@ -0,0 +1,17 @@
+class Mcpify < Formula
+  desc "API endpoints as MCP tools"
+  homepage "https://github.com/NilayYadav/mcpify"
+  url "https://github.com/NilayYadav/mcpify/archive/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "MIT"
+  
+  depends_on "go" => :build
+  
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+  
+  test do
+    system "#{bin}/mcpify", "--version"
+  end
+end
