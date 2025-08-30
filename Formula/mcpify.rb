7c360f2f8090a714a0a533f8668b41de1aea9113
Nilay Yadav <nilayyadav10@gmail.com>
2025-08-30 21:50:32 +0530

    1.0.6

--- Formula/mcpify.rb
+++ Formula/mcpify.rb
@@ -1,7 +1,7 @@
 class Mcpify < Formula
   desc "API endpoints as MCP tools"
   homepage "https://github.com/NilayYadav/mcpify"
-  url "https://github.com/NilayYadav/mcpify/archive/v1.0.0.tar.gz"
+  url "https://github.com/NilayYadav/mcpify/archive/v1.0.6.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   
